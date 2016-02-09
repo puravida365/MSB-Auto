f124f170e5e4e3ec5b05afb98e59bbc15add4c9a
Josue Mora <josue@inno-live.com>
2016-02-08 18:53:17 -0800

    Styled navigation for mobile and desktop, added Jquery and Bootstrap JS CDN

--- src/app/about/about.js
+++ src/app/about/about.js
@@ -8,6 +8,12 @@
         function init() {
             // A definitive place to put everything that needs to run when the controller starts. Avoid
             //  writing any code outside of this function that executes immediately.
+
+
+            $(".navbar-nav li a").click(function(event) {
+                $(".navbar-collapse").collapse('hide');
+            });
+
         }
 
     });
